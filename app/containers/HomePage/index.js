fd3abada3228bdadd3747315ee68750fc9fddefa
Mahefa M <mahefa.manitrarivo@gmail.com>
2017-05-17 17:34:13 +0200

    Adding API

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,30 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+// import { sendPayload } from 'utils/api';
+
+const ttn = require('ttn');
+const _get = require('lodash/get');
+const region = 'eu';
+const appId = 'bike-lights';
+const accessKey = 'ttn-account-v2.plUG-8dTQ79tIV_CrQNcCzdREaav3DA3iUqWHxyhlkM';
+const deviceID = 'thethingsuno-didac';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
-  }
+
+    constructor() {
+        super();
+        this.client = new ttn.Client(region, appId, accessKey);
+        this.client.on('connect', function(connack) {
+            console.log('[DEBUG]', 'Connect:', connack);
+        });
+    }
+
+    render() {
+        return (
+            <h1>
+                <button onClick={() => this.client.send(deviceID, '11')}>Click me</button>
+            </h1>
+        );
+    }
 }
